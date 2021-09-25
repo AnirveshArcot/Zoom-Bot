9bbdba1e8f7638ddecb1b42938967bbfce565c3c
Cyanide <77383465+AnirveshArcot@users.noreply.github.com>
2021-09-25 18:55:21 +0530

    First commit

---
+++ main.py
@@ -0,0 +1,57 @@
+import subprocess
+import pyautogui
+import time
+from datetime import datetime
+import pandas as pd 
+
+def login(classlink):
+
+    subprocess.call([ "C:\Program Files\Google\Chrome\Application\chrome.exe"])
+    time.sleep(2)
+    pyautogui.write(classlink)
+    pyautogui.press('enter')
+    time.sleep(5)
+
+    launch_btn=pyautogui.locateOnScreen('lmeet_dark.png',grayscale=False, confidence=.5)
+    pyautogui.moveTo(launch_btn)
+    pyautogui.click()
+
+    launch_btn=pyautogui.locateOnScreen('lmeet.png',grayscale=False, confidence=.5)
+    pyautogui.moveTo(launch_btn)
+    pyautogui.click()
+    time.sleep(1)
+
+    joinw_btn= pyautogui.locateCenterOnScreen('joinbrow_w.PNG')
+    pyautogui.moveTo(joinw_btn)
+    pyautogui.click()
+
+    joinb_btn= pyautogui.locateCenterOnScreen('joinbrow_b.PNG')
+    pyautogui.moveTo(joinb_btn)
+    pyautogui.click()
+    time.sleep(2)
+
+    joinw_btn= pyautogui.locateCenterOnScreen('join_w.png',grayscale=False)
+    pyautogui.moveTo(joinw_btn)
+    pyautogui.click()
+
+    joinb_btn= pyautogui.locateCenterOnScreen('join_b.png',grayscale=False)
+    pyautogui.moveTo(joinb_btn)
+    pyautogui.click()
+    
+    audio_btn=pyautogui.locateCenterOnScreen('inmeet.png',grayscale=False,)
+    pyautogui.moveTo(audio_btn)
+    pyautogui.click()
+    time.sleep(10)
+
+data=pd.read_csv('timing.csv')
+
+while True:
+
+    timern=datetime.now().strftime("%H:%M")
+    if timern in str(data['time(24hrformat)']):
+        row=data.loc[data['time(24hrformat)'] == timern]
+        link= str(row.iloc[0,1])
+        login(link)
+        time.sleep(20)
+        print("Signed In")
+
